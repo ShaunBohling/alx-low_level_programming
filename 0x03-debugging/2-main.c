321e983f0475445d6b2db2af6a5911f150e03e13
Your Name <you@example.com>
2023-02-23 11:02:48 -0800

    Task 2

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -2,6 +2,33 @@
 #include "main.h"
 
 /**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	if (a >= b && a >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && b >= c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+
+	return (largest);
+}
+/**
 * main - prints the largest of 3 integers
 * Return: 0
 */
@@ -10,7 +37,7 @@ int main(void)
 {
 	int a, b, c;
 	int largest;
-	
+
 	a = 972;
 	b = -98;
 	c = 0;
