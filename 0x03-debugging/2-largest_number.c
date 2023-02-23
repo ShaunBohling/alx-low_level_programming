182f4b37336544da0a5bdf4878685085cf4ef613
Your Name <you@example.com>
2023-02-23 01:43:32 -0800

    Task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,17 +10,14 @@
 
 int largest_number(int a, int b, int c)
 {
+
 	int largest;
 
 	if (a > b && a > c)
 	{
 		largest = a;
 	}
-	else if (a > b && c > a)
-	{
-		largest = c;
-	}
-	else if (b > c)
+	else if (b > a && b > c)
 	{
 		largest = b;
 	}
