98d3761d54891e434b7c25d106c8a796f6946c33
Your Name <you@example.com>
2023-02-24 02:07:27 -0800

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,8 +8,16 @@ void fizz_buzz(void)
 {
 	int x;
 
-	for (x = 0; x < 100; x++)
+	for (x = 1; x < 100; x++)
 	{
-		printf("%d ", x);
+		if (x % 3 == 0 && x % 5 == 0)
+			printf("Fizz ");
+		else if (x % 3 == 0)
+			printf("Buzz ");
+		else if (x % 5 == 0)
+			printf("FizzBuzz")
+		else
+			printf("%d ", x);
 	}
+	printf("\n");
 }
