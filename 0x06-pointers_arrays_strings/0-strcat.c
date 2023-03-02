944186b7882f415ce222f846e36cae919aa2353e
Your Name <you@example.com>
2023-03-02 04:15:00 -0800

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,25 +1,29 @@
 #include "main.h"
 /**
  * _strcat - concatenates two strings
- * @dest: pointer destination
- * @src: pointer source
+ * @dest: input value
+ * @src: input value
+ *
  * Return: void
  */
-
-char *_strcat(cat *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
-	int a = -1, i;
+	int i;
+	int j;
 
-	for (i = 0; dest[i] != '\0';)
+	i 0;
+	while (dest[i] != '\0')
+	{
 		i++;
-
-	do {
-		a++;
-		dest[i] = src[a];
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
 		i++;
+		j++;
+	}
 
-	} while (src[a] != '\0');
-
+	dest[i] = '\0';
 	return (dest);
-
 }
